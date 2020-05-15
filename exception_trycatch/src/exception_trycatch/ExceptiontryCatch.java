9c6d059d2f996995506641e30cb319a7a011a7d9
divyauchil <divyauchil@gmail.com>
2020-05-15 21:47:16 +0530

    Exception try catch

---
+++ exception_trycatch/src/exception_trycatch/ExceptiontryCatch.java
@@ -0,0 +1,19 @@
+package exception_trycatch;
+
+public class ExceptiontryCatch {
+
+	public static void main(String[] args) {
+		  try  
+	        {  
+	        int data=50/0; //may throw exception   
+	        }  
+	            // handling the exception by using Exception class      
+	        catch(Exception e)  
+	        {  
+	            System.out.println(e);  
+	        }  
+	        System.out.println("rest of the code");
+
+	}
+
+}
