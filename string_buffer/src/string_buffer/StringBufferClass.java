994b73d3d4832c2a331268604ecdd157c1d07662
divyauchil <divyauchil@gmail.com>
2020-05-15 23:14:34 +0530

    String buffer class

---
+++ string_buffer/src/string_buffer/StringBufferClass.java
@@ -0,0 +1,12 @@
+package string_buffer;
+
+public class StringBufferClass {
+
+	public static void main(String[] args) {
+		StringBuffer sBuffer = new StringBuffer("test");
+	      sBuffer.append(" String Buffer");
+	      System.out.println(sBuffer);
+
+	}
+
+}
