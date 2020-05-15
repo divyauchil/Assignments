36bfbd74e1968d17f548797782ac99f28b854eb5
divyauchil <divyauchil@gmail.com>
2020-05-15 19:21:40 +0530

    thread runnable interface

---
+++ thread_runnable/src/thread_runnable/ThreadRunnable.java
@@ -0,0 +1,19 @@
+package thread_runnable;
+
+
+
+
+	
+	class ThreadRunnable implements Runnable{  
+		public void run(){  
+		System.out.println("thread is running...");  
+		}  
+
+	public static void main(String[] args) {
+		ThreadRunnable m1=new ThreadRunnable();  
+		Thread t1 =new Thread(m1);  
+		t1.start(); 
+
+	}
+
+}
