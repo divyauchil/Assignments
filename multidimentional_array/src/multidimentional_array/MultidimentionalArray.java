6df502b7fc046fb08e6cbe4bea1641fc15ae95da
divyauchil <divyauchil@gmail.com>
2020-05-15 23:24:29 +0530

    multidimentional array

---
+++ multidimentional_array/src/multidimentional_array/MultidimentionalArray.java
@@ -0,0 +1,13 @@
+package multidimentional_array;
+
+public class MultidimentionalArray {
+
+	public static void main(String[] args) {
+		int[][] arr = new int[10][20]; 
+        arr[0][0] = 1; 
+  
+        System.out.println("arr[0][0] = " + arr[0][0]); 
+
+	}
+
+}
