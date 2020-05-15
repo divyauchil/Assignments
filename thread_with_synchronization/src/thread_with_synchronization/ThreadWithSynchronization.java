8efc54db816edebb0317597c55869608f1adf625
divyauchil <divyauchil@gmail.com>
2020-05-15 20:00:32 +0530

    thread with synchronization

---
+++ thread_with_synchronization/src/thread_with_synchronization/ThreadWithSynchronization.java
@@ -0,0 +1,54 @@
+package thread_with_synchronization;
+
+class First
+{
+ public void display(String msg)
+ {
+  System.out.print ("["+msg);
+  try
+  {
+   Thread.sleep(1000);
+  }
+  catch(InterruptedException e)
+  {
+   e.printStackTrace();
+  }
+  System.out.println ("]");
+ }
+}
+
+class Second extends Thread
+{
+ String msg;
+ First fobj;
+ Second (First fp,String str)
+ {
+  fobj = fp;
+  msg = str;
+  start();
+ }
+ public void run()
+ {
+  synchronized(fobj)      //Synchronized block
+  {
+   fobj.display(msg);
+  }
+ }
+}
+
+
+
+
+
+
+public class ThreadWithSynchronization {
+
+	public static void main(String[] args) {
+		First fnew = new First();
+		  Second ss = new Second(fnew, "welcome");
+		  Second ss1= new Second (fnew,"new");
+		  Second ss2 = new Second(fnew, "programmer");
+
+	}
+
+}
