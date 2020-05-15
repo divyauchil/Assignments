56c14c98160fb80983e386800eb65d4b38f1018d
divyauchil <divyauchil@gmail.com>
2020-05-15 22:45:27 +0530

    Diamond problem

---
+++ diamond_problem/src/diamond_problem/DiamondProblem.java
@@ -0,0 +1,30 @@
+package diamond_problem;
+
+interface MyInterface1{
+	   public static int num = 100;
+	   public default void display() {
+	      System.out.println("display method of MyInterface1");
+	   }
+	}
+	interface MyInterface2{
+	   public static int num = 1000;
+	   public default void display() {
+	      System.out.println("display method of MyInterface2");
+	   }
+	}
+
+public class DiamondProblem implements MyInterface1, MyInterface2{
+	
+	public void display() {
+	      MyInterface1.super.display();
+	      //or,
+	      MyInterface2.super.display();
+	   }
+
+	public static void main(String[] args) {
+		DiamondProblem obj = new DiamondProblem();
+	      obj.display();
+
+	}
+
+}
