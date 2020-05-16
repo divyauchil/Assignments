dbe08758375276b7f5ca604a959a00f72cc1fbec
divyauchil <divyauchil@gmail.com>
2020-05-16 09:08:14 +0530

    leap year

---
+++ leap_year/src/leapyear/LeapYear.java
@@ -0,0 +1,20 @@
+package leapyear;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		int year;
+        
+        System.out.println("Enter year");
+        Scanner sc = new Scanner(System.in);
+        year = sc.nextInt();
+        sc.close();
+        if (((year % 4 == 0) && (year % 100!= 0)) || (year % 400 == 0))
+            System.out.println("Year is a leap year");
+         else
+            System.out.println("Year is not a leap year");
+	}
+
+}
